a8b5965f48354acd6967cd040bf7cbca7ba30150
DeerEyre <837074044@qq.com>
2022-03-09 23:40:15 +0800

    ganshu

--- wit120front/src/main.js
+++ wit120front/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// import { Button, Select } from 'element-ui';
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
-
+// Vue.component(Button.name, Button);
+// Vue.component(Select.name, Select);
 new Vue({
   router,
   render: h => h(App)
--- wit120front/src/views/AboutView.vue
+++ wit120front/src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="login-wrap">
+        <el-form class="login-container">
+            <h1 class="title">用户登录</h1>
+            <el-form-item label="">
+                <el-input type="text" v-model="username" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="">
+                <el-input type="password" v-model="password" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" style="width: 100%;" @click="doSubmit()">提交</el-button>
+            </el-form-item>
+            <el-row style="text-align: center; margin-top: -10px;;">
+                <el-link type="primary">忘记密码</el-link>
+                <el-link type="primary" @click="doRegister()">用户注册</el-link>
+            </el-row>
+        </el-form>
+    </div>
 </template>
