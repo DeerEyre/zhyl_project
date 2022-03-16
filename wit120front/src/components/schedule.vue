020a6c9baab75fd032960fd37c63005d2b6a679b
DeerEyre <837074044@qq.com>
2022-03-16 11:42:51 +0800

    master

---
+++ wit120front/src/components/schedule.vue
@@ -0,0 +1,56 @@
+  <template>
+  <div>
+    <el-table
+      :data="tableData"
+      style="width: 100%" @cell-click="handle">
+      <el-table-column
+        prop="doctor"
+        label="医师"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="department"
+        label="科室"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="habit"
+        label="资历">
+      </el-table-column>
+    </el-table>
+  </div>
+  </template>
+
+  <script>
+    export default {
+      data() {
+        return {
+          tableData: [{
+            doctor: '甘舒',
+            department: '内科',
+            habit: '英语六级430分'
+          }, {
+            doctor: '腰腰',
+            department: '外科',
+            habit: '有瑶的情人节皮肤'
+          }, {
+            doctor: '巴子',
+            department: '耳鼻喉科',
+            habit: '未来群主'
+          }, {
+            doctor: '硕硕',
+            department: '妇科',
+            habit: '野爹'
+          }]
+        }
+      },
+      methods: {
+        handle(row,column,event,cell){
+        console.log(row)
+        console.log(column)
+        console.log(event)
+        console.log(cell)
+    }
+  }
+    }
+  </script>
